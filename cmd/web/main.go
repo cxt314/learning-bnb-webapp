5bdc52f71fde6092e27678e943b558f5c074829e
cxt314 <103610756+cxt314@users.noreply.github.com>
2023-01-06 13:43:06 -0600

    add helpers package for error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 
 	"github.com/cxt314/learning-bnb-webapp/internal/config"
 	"github.com/cxt314/learning-bnb-webapp/internal/handlers"
+	"github.com/cxt314/learning-bnb-webapp/internal/helpers"
 	"github.com/cxt314/learning-bnb-webapp/internal/models"
 	"github.com/cxt314/learning-bnb-webapp/internal/render"
 )
@@ -19,6 +21,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -48,6 +52,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -67,8 +77,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
--- internal/forms/forms_test.go
+++ internal/forms/forms_test.go
@@ -17,27 +17,111 @@ func TestForm_Valid(t *testing.T) {
 }
 
 func TestForm_Required(t *testing.T) {
-	r := httptest.NewRequest("POST", "/test-url", nil)
-	form := New(r.PostForm)
+	postedData := url.Values{}
+	form := New(postedData)
 
 	form.Required("a", "b", "c")
 	if form.Valid() {
 		t.Error("Form shows valid when required fields are missing")
 	}
 
-	postedData := url.Values{}
+	postedData = url.Values{}
 	postedData.Add("a", "a")
 	postedData.Add("b", "b")
 	postedData.Add("c", "c")
 
-	r = httptest.NewRequest("POST", "/test-url", nil)
-
-	r.PostForm = postedData
-	form = New(r.PostForm)
+	form = New(postedData)
 
 	form.Required("a", "b", "c")
 	if !form.Valid() {
 		t.Error("Form shows 'does not have required fields' when it does.")
+	}
+
+}
+
+func TestForm_Has(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	if form.Has("a") {
+		t.Error("Form shows 'has field' when field is missing")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "a")
+
+	form = New(postedData)
+
+	if !form.Has("a") {
+		t.Error("Form shows 'does not have field' when field exists")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	if form.MinLength("a", 3) {
+		t.Error("Form shows 'min length valid' when field does not exist")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "a")
+
+	form = New(postedData)
+
+	if form.MinLength("a", 3) {
+		t.Error("Form shows 'min length valid' when field is too short")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "abc")
+
+	form = New(postedData)
+
+	if !form.MinLength("a", 3) {
+		t.Error("Form shows 'min length Invalid' when field is long enough")
+	}
+
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	postedData := url.Values{}
+	form := New(postedData)
+
+	form.IsEmail("a")
+	if form.Valid() {
+		t.Error("Form shows 'email valid' when field does not exist")
+	}
+
+	isError := form.Errors.Get("a")
+	if isError == "" {
+		t.Error("Should be an error but did not get one")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "a")
+
+	form = New(postedData)
+
+	form.IsEmail("a")
+	if form.Valid() {
+		t.Error("Form shows 'email valid' when field is not an email")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("a", "test@email.com")
+
+	form = New(postedData)
+
+	form.IsEmail("a")
+	if !form.Valid() {
+		t.Error("Form shows 'Invalid email' when field is an email")
+	}
+
+	isError = form.Errors.Get("a")
+	if isError != "" {
+		t.Error("Should not have an error, but got one")
 	}
 
 }
